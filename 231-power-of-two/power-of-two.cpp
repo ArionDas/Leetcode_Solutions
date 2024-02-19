42a0da54e2c167cfd51d0401ee6c0bfb4b1cf9b3
Arion Das <117722561+ArionDas@users.noreply.github.com>
2024-02-19 12:35:25 +0530

    Time: 3 ms (26.38%) | Memory: 7.1 MB (39.77%) - LeetSync

---
+++ 231-power-of-two/power-of-two.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        int c=0;
+        while(n>0) {
+            if(n&1) c++;
+            n>>=1;
+        }
+        if(c==1) return 1;
+        return 0;
+    }
+};
