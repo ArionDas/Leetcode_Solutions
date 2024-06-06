5bab8cf03d6e75c8c4b270c387844b16f9da977d
Arion Das <117722561+ArionDas@users.noreply.github.com>
2024-06-06 08:44:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (99.53%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int rob = 0;
-        int norob = 0;
-        for(int i=0; i<nums.size(); i++) {
-            int newRob = norob + nums[i];
-            int newNoRob = max(norob, rob);
-            rob = newRob;
-            norob = newNoRob;
+        int curr = nums[0];
+        int prev = nums[0];
+        int prev_2 = 0;
+
+        for(int i=1; i<nums.size(); i++) {
+            int take = nums[i] + prev_2;
+            int not_take = 0 + prev;
+
+            curr = max(take, not_take);
+            prev_2 = prev;
+            prev = curr;
         }
-        return max(rob, norob);
+
+        return prev;
     }
 };
