2f8841ee2155c0d2fab1162fc51ebf9e1357f49a
Arion Das <117722561+ArionDas@users.noreply.github.com>
2024-06-20 09:59:15 +0530

    Time: 6 ms (19.92%) | Memory: 10.2 MB (35.05%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -25,11 +25,24 @@ public:
         if(m==1 && n==1) {
             if(obs[0][0]==1)
                 return 0;
-            else
-                return 1;
         }
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        int ans = count_ways(0, 0, m, n, obs, dp);
-        return ans;
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return count_ways(0, 0, m, n, obs, dp);
+        // vector<int> prev(m,0);
+        // for(int i=0; i<m; i++) {
+        //     vector<int> curr(n, 0);
+        //     for(int j=0; j<n; j++) {
+        //         if(i==0 && j==0) curr[j] = 1;
+        //         else {
+        //             int up = 0;
+        //             int left = 0;
+        //             if(i>0) up = prev[j];
+        //             if(j>0) left = curr[j-1];
+        //             curr[j] = up + left;
+        //         }
+        //     }
+        //     prev = curr;
+        // }
+        // return prev[n-1];
     }
 };
