c210088da1bbaa4be0f3f95945968f21bdb800ee
Arion Das <117722561+ArionDas@users.noreply.github.com>
2024-06-20 09:02:46 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (71.64%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,25 +1,36 @@
 class Solution {
 public:
+    // int count_ways(int a, int b, int m, int n, vector<vector<int>> &dp) {
+    //     if(a==m-1 && b==n-1) {
+    //         return 1;
+    //     }
+    //     if(dp[a][b] != -1) return dp[a][b];
+    //     int ways = 0;
+    //     if(a+1<m) {
+    //         ways += count_ways(a+1, b, m, n, dp);
+    //     }
+    //     if(b+1<n) {
+    //         ways += count_ways(a, b+1, m, n, dp);
+    //     }
 
-    int count_ways(int a, int b, int m, int n, vector<vector<int>> &dp) {
-        if(a==m-1 && b==n-1) {
-            return 1;
-        }
-        if(dp[a][b] != -1) return dp[a][b];
-        int ways = 0;
-        if(a+1<m) {
-            ways += count_ways(a+1, b, m, n, dp);
-        }
-        if(b+1<n) {
-            ways += count_ways(a, b+1, m, n, dp);
-        }
-
-        return dp[a][b] = ways;
-    }
-
+    //     return dp[a][b] = ways;
+    // }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        int ans = count_ways(0, 0, m, n, dp);
-        return ans;
+        vector<int> prev(m,0);
+        for(int i=0; i<m; i++) {
+            vector<int> curr(n, 0);
+            for(int j=0; j<n; j++) {
+                if(i==0 && j==0) curr[j] = 1;
+                else {
+                    int up = 0;
+                    int left = 0;
+                    if(i>0) up = prev[j];
+                    if(j>0) left = curr[j-1];
+                    curr[j] = up + left;
+                }
+            }
+            prev = curr;
+        }
+        return prev[n-1];
     }
 };
