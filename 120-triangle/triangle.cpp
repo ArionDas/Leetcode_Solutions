496085f55f8a485bdfd81968aa4a08f6cd2f5e6f
Arion Das <117722561+ArionDas@users.noreply.github.com>
2024-06-27 09:10:29 +0530

    Time: 4 ms (73.16%) | Memory: 11.3 MB (71.66%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,24 +1,39 @@
 class Solution {
 public:
 
-    int find_min_path_sum(int i, int j, int n, vector<vector<int>>& triangle, vector<vector<int>>& dp) {
-        if(dp[i][j]!=-1)
-            return dp[i][j];
+    // int find_min_path_sum(int i, int j, int n, vector<vector<int>>& triangle, vector<vector<int>>& dp) {
+    //     if(dp[i][j]!=-1)
+    //         return dp[i][j];
 
-        if(i==n-1) {
-            return triangle[i][j];
+    //     if(i==n-1) {
+    //         return triangle[i][j];
+    //     }
+
+    //     int bottom = find_min_path_sum(i+1, j, n, triangle, dp);
+    //     int right = find_min_path_sum(i+1, j+1, n, triangle, dp);
+
+    //     return dp[i][j] = min(bottom, right) + triangle[i][j];
+    // }
+
+    int minimumTotal(vector<vector<int>>& tri) {
+        int n = tri.size();
+        vector<int> prev(n,0);
+
+        for(int j=0; j<n; j++) {
+            prev[j] = tri[n-1][j];
         }
 
-        int bottom = find_min_path_sum(i+1, j, n, triangle, dp);
-        int right = find_min_path_sum(i+1, j+1, n, triangle, dp);
+        for(int i=n-2; i>=0; i--) {
+            vector<int> curr(n,0);
 
-        return dp[i][j] = min(bottom, right) + triangle[i][j];
-    }
+            for(int j=i; j>=0; j--) {
+                int down = tri[i][j] + prev[j];
+                int diagonal = tri[i][j] + prev[j+1];
+                curr[j] = min(down, diagonal);
+            }
+            prev = curr;
+        }
 
-    int minimumTotal(vector<vector<int>>& triangle) {
-        int n = triangle.size();
-        vector<vector<int>> dp(n, vector<int>(n,-1));
-
-        return find_min_path_sum(0, 0, n, triangle, dp);
+        return prev[0];
     }
 };
