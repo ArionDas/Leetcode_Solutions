3ab353a11e59a17cea57c57650f0b2e0d35ffc86
Arion Das <117722561+ArionDas@users.noreply.github.com>
2024-07-08 09:19:28 +0530

    Time: 15 ms (93.49%) | Memory: 7.8 MB (90.07%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -4,20 +4,21 @@ public:
         int text1_len = text1.length();
         int text2_len = text2.length();
 
-        vector<vector<int>> dp(text1_len+1, vector(text2_len+1, 0));
+        vector<int> prev(text2_len+1, 0), curr(text2_len+1, 0);
         //int dp[text1_len+1][text2_len+1]={0};
 
         for(int i=1; i<=text1_len; i++) {
             for(int j=1; j<=text2_len; j++) {
                 if(text1[i-1]==text2[j-1]) {
-                    dp[i][j] = dp[i-1][j-1] + 1;
+                    curr[j] = prev[j-1] + 1;
                 }
                 else {
-                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                    curr[j] = max(prev[j],curr[j-1]);
                 }
             }
+            prev = curr;
         }
 
-        return dp[text1_len][text2_len];
+        return curr[text2_len];
     }
 };
