84d4d1a938b50b65996387f860bbf8b6d8de570b
Arion Das <117722561+ArionDas@users.noreply.github.com>
2024-07-20 12:07:12 +0530

    Time: 127 ms (65.60%) | Memory: 65.6 MB (11.89%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,44 +1,40 @@
 class Solution {
 public:
 
-    bool dfsCheck(int node, vector<vector<int>> &adj, vector<int> &vis, vector<int> &pathvis, vector<int> &check) {
-        vis[node] = 1;
-        pathvis[node] = 1;
-        check[node] = 0;
-        
-        //traverse for adjacent nodes
-        for(auto it:adj[node]) {
-            // when the node is not visited
-            if(!vis[it]) {
-                if(dfsCheck(it, adj, vis, pathvis, check)==true) {
-                    check[node] = 0;
-                    return true;
-                }
-            }
-            else if(pathvis[it]) {
-                check[node] = 0;
-                return true;
-            }
-        }
-        check[node] = 1; // if a dfs successfully completes for a node, it's a safe node
-        pathvis[node] = 0;
-        return false;
-    }
+    vector<int> eventualSafeNodes(vector<vector<int>>& adj) {
+        int V = adj.size();
+        vector<int> safe_nodes;
 
-    vector<int> eventualSafeNodes(vector<vector<int>>& grid) {
-        int V = grid.size();
-        vector<int> vis(V,0);
-        vector<int> pathvis(V,0);
-        vector<int> check(V,0);
-        vector<int> safenodes;
+        vector<int> indg(V, 0);
+        vector<vector<int>> reverse_edges(V);
         for(int i=0; i<V; i++) {
-            if(!vis[i]) {
-                dfsCheck(i, grid, vis, pathvis, check);
+            for(auto it:adj[i]) {
+                indg[i]++;
+                reverse_edges[it].push_back(i);
             }
         }
-        for(int i=0; i<V; i++)
-            if(check[i]==1) safenodes.push_back(i);
-            
-        return safenodes;
+
+        queue<int> q;
+        for(int i=0; i<V; i++) {
+            if(indg[i]==0)
+                q.push(i);
+        }
+
+        vector<int> topo;
+        while(!q.empty()) {
+            int node = q.front();
+            q.pop();
+
+            topo.push_back(node);
+
+            for(auto it:reverse_edges[node]) {
+                indg[it]--;
+                if(indg[it]==0)
+                    q.push(it);
+            }
+        }
+
+        sort(topo.begin(), topo.end());
+        return topo;
     }
 };
